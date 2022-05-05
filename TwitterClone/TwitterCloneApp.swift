cec3531e7ba32a1f90cdbcd41de4296a546e82be
99Page <nou0ggid@gmail.com>
2022-05-05 22:10:22 +0900

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by 노우영 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
