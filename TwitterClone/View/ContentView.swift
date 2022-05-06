6918bfdbb86a766d7c98f8ed0be7830f074ebbb6
99Page <nou0ggid@gmail.com>
2022-05-06 16:02:15 +0900

    first commit

---
+++ TwitterClone/View/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by 노우영 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    
+    var body: some View {
+        NavigationView {
+            TabView {
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                
+                
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Message")
+                    }
+                
+                
+
+            }.navigationTitle("홈 화면")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterClone/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  TwitterClone
+//
+//  Created by 노우영 on 2022/05/06.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
