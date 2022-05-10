86fc306858994c67b371f4587d161a6d0656886b
99Page <nou0ggid@gmail.com>
2022-05-10 15:51:14 +0900

    chat mockup data

---
+++ TwitterClone/Model/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  TwitterClone
+//
+//  Created by 노우영 on 2022/05/10.
+//
+
+import Foundation
+
+struct MockMessage: Identifiable {
+    let id: Int
+    let imageName: String
+    let massageText: String
+    let isCurrentUser: Bool
+}
+
+
--- TwitterClone/View/Message/ChatView.swift
+++ TwitterClone/View/Message/ChatView.swift
@@ -11,19 +11,48 @@ struct ChatView: View {
     
     @State var messageText: String = ""
     
+    @State var MOCK_MESSAGES: [MockMessage] = [
+        .init(id: 1, imageName: "person", massageText: "Hey what`s up?", isCurrentUser: false),
+        .init(id: 2, imageName: "applelogo", massageText: "Not much, you?", isCurrentUser: true),
+        .init(id: 3, imageName: "applelogo", massageText: "How`s the marvel universe?", isCurrentUser: true),
+        .init(id: 4, imageName: "person", massageText: "It`s pretty good dude", isCurrentUser: false),
+        .init(id: 5, imageName: "person", massageText: "How`s your battle with the joker?", isCurrentUser: false),
+        .init(id: 6, imageName: "applelogo", massageText: "Not going so hot...", isCurrentUser: true),
+    ]
+    
     var body: some View {
         VStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 12) {
-                    ForEach(0..<15) { _ in
+                    ForEach(MOCK_MESSAGES) { message in
                         HStack {
-                            Spacer()
-                            Text("Test meesage text")
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .clipShape(ChatBubble(isFromCurrentUser: true))
-                                .padding(.horizontal)
+                            if message.isCurrentUser {
+                                Spacer()
+                                Text(message.massageText)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .clipShape(ChatBubble(isFromCurrentUser: true))
+                                    .padding(.horizontal)
+                            } else {
+                                HStack(spacing: 0) {
+                                    Image(systemName: message.imageName)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width:40, height: 40)
+                                        .clipShape(Circle())
+                                        .padding(.leading, 5)
+                                    
+                                    Text(message.massageText)
+                                        .padding()
+                                        .background(Color(.systemGray4))
+                                        .clipShape(ChatBubble(isFromCurrentUser: false))
+                                        .padding(.horizontal)
+                                    
+                                }
+                                Spacer()
+                            }
+                          
                         }
                         
                     }
