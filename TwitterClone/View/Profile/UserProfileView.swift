98949539901bcf33829aa50a7567939b1b6dd859
99Page <nou0ggid@gmail.com>
2022-05-16 21:22:54 +0900

    filter button

--- TwitterClone/View/Profile/UserProfileView.swift
+++ TwitterClone/View/Profile/UserProfileView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    
+    @State var selectedFilter: TweetFilterOptions = .tweets
+    
     var body: some View {
         ScrollView {
             VStack {
                 ProfileHeaderView()
+                    .padding()
+                
+                FilterButtonView(selectedOption: $selectedFilter)
             }
         }
     }
