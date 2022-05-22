bbbb05e615a46d2fa3bc823e33efd8622601ccac
99Page <nou0ggid@gmail.com>
2022-05-22 22:42:54 +0900

    photo picker

--- TwitterClone/View/Authentication/LoginView.swift
+++ TwitterClone/View/Authentication/LoginView.swift
@@ -19,79 +19,80 @@ struct LoginView: View {
     
     var body: some View {
         
-        ZStack {
-            VStack {
-                Image("twitterLogo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 250, height: 100)
-                    .clipped()
-                    .padding(.top, 60)
-                
-                VStack(spacing: 15) {
-                    CustomTextField(text: $email, placeholder: Text(emailPlaceholder), imageSource: emailImage)
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image("twitterLogo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 250, height: 100)
+                        .clipped()
+                        .padding(.top, 60)
                     
-                    CustomTextField(text: $password, placeholder: Text(passwordPlaceholder), imageSource: passwordImage)
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                }
-                .padding()
-                
-                HStack {
+                    VStack(spacing: 20) {
+                        CustomTextField(text: $email, placeholder: Text(emailPlaceholder), imageSource: emailImage)
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                        
+                        CustomTextField(text: $password, placeholder: Text(passwordPlaceholder), imageSource: passwordImage)
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Forget Password?")
+                                .foregroundColor(.white)
+                                .font(.footnote)
+                                .bold()
+                        }
+
+                    }
+                    .padding(.horizontal)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .frame(width: 360, height: 50)
+                            .background(Color.white)
+                            .clipShape(Capsule())
+                            .padding()
+                    }
+                    
                     Spacer()
                     
-                    Button {
-                        
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
                     } label: {
-                        Text("Forget Password?")
-                            .foregroundColor(.white)
-                            .font(.footnote)
-                            .bold()
+                        HStack {
+                            Text("Don`t have an account?")
+                                .font(.system(size: 14))
+                            
+                            Text("Sign up")
+                                .font(.system(size: 14, weight: .semibold))
+                            
+                        }
+                        .padding(.bottom, 40)
+                        .foregroundColor(.white)
                     }
-
                 }
-                .padding(.horizontal)
+           
                 
-                Button {
-                    
-                } label: {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .frame(width: 360, height: 50)
-                        .background(Color.white)
-                        .clipShape(Capsule())
-                        .padding()
-                }
-                
-                Spacer()
-                
-                HStack {
-                    Text("Don`t have an account?")
-                        .font(.system(size: 14))
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("Sign up")
-                            .font(.system(size: 14))
-                            .bold()
-                    }
-
-                    
-                }
-                .padding(.bottom, 40)
-                .foregroundColor(.white)
             }
-       
-            
-        }
-        .background(Color.twitterBackgound)
+            .background(Color.twitterBackgound)
         .ignoresSafeArea()
+        }
     }
 }
 
--- TwitterClone/View/Authentication/RegistrationView.swift
+++ TwitterClone/View/Authentication/RegistrationView.swift
@@ -8,8 +8,103 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State var binding : [String] = ["", "", "", ""]
+    let placeholder  = ["Full name", "Email", "Username", "Password"]
+    let imageSource = ["person", "envelope", "person", "lock"]
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @State var showImagePicker = false
+    @State var selectedUIImage: UIImage?
+    @State var image: Image?
+    
+    func loadImage() {
+        guard let selectedImage = selectedUIImage else { return }
+        image = Image(uiImage: selectedImage)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                
+                Button {
+                    showImagePicker.toggle()
+                } label: {
+                    ZStack {
+                        if let image = image {
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 120, height: 120)
+                                .clipped()
+                                .cornerRadius(20)
+                                
+                        } else {
+                            Image(systemName: "plus.circle")
+                                .resizable()
+                                .frame(width: 120, height: 120)
+                                .clipShape(Circle())
+                        }
+                    }
+                 
+                }
+                .padding(.top, 40)
+                .foregroundColor(.white)
+                .sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
+                    ImagePicker(image: $selectedUIImage)
+                }
+
+                
+                VStack(spacing: 20) {
+                    ForEach(0 ..< 4) { i in
+                        CustomTextField(text: $binding[i], placeholder: Text(placeholder[i]), imageSource: imageSource[i])
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                    }
+                }
+                .padding()
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    mode.wrappedValue.dismiss()
+                }, label: {
+                    HStack {
+                        Text("Already hava an account?")
+                            .font(.system(size: 14))
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Sign up")
+                                .font(.system(size: 14))
+                                .bold()
+                        }
+
+                        
+                    }
+                })
+                    .padding(.bottom, 40)
+                    .foregroundColor(.white)
+            }
+       
+            
+        }
+        .background(Color.twitterBackgound)
+        .ignoresSafeArea()
     }
 }
 
