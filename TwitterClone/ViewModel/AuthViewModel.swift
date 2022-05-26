3e16afdc8c9822aeaece25d6dafcc91d343210cb
99Page <nou0ggid@gmail.com>
2022-05-26 20:25:29 +0900

    user registration

--- TwitterClone/ViewModel/AuthViewModel.swift
+++ TwitterClone/ViewModel/AuthViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import FirebaseStorage
 
 class AuthViewModel: ObservableObject {
     
@@ -15,7 +16,39 @@ class AuthViewModel: ObservableObject {
     }
     
     func registerUser(email: String, password: String, username: String, fullname: String, profileImage: UIImage) {
-        print("DEBUG: Email is \(email)")
-        print("DEBUG: Password is \(password)")
+        
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
+        
+        storageRef.putData(imageData, metadata: nil) { _, error in
+            if let error = error {
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
+                return
+            }
+            
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let user = result?.user else { return }
+                    
+                    let data = ["email": email,
+                                "username": username,
+                                "fullname": fullname,
+                                "profileImageUrl": profileImageUrl,
+                                "uid": user.uid]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        print("DEBUG: Succesfully uploaded user data...")
+                    }
+                }
+            }
+        }
     }
 }
